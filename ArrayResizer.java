9408197999e69c6ab0d71d747eba1f18b1b61e05
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-04-23 13:31:01 +0000

    initialize

---
+++ ArrayResizer.java
@@ -0,0 +1,21 @@
+public class ArrayResizer
+{
+
+public static boolean isNonZeroRow(int[][] array2D, int r)
+{
+     for(int i : array2D[r])
+     {
+        if(i== 0)return false;
+     }
+     return true;
+
+}
+
+
+public static int numNonZeroRows(int[][] array2D)
+{ return 0;}
+
+
+public static int[][] resize(int[][] array2D)
+{ return null; }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+    int[][] arr = {{2, 1, 0},
+                    {1, 3, 2},
+{0, 0, 0},
+{4, 5, 6}};
+    }
+}
