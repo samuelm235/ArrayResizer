24b23433a2a8b1a28c828ef3c4bc1c90062f8ed4
samuelm235 <108957962+samuelm235@users.noreply.github.com>
2025-04-25 13:32:23 +0000

    done

--- ArrayResizer.java
+++ ArrayResizer.java
@@ -13,9 +13,29 @@ public static boolean isNonZeroRow(int[][] array2D, int r)
 
 
 public static int numNonZeroRows(int[][] array2D)
-{ return 0;}
+{ 
+   int k = 0;
+   for(int i = 0; i < array2D.length; i++)
+   {
+   if(isNonZeroRow(array2D, i)) k ++;
+   }
+   return k;
+}
 
 
 public static int[][] resize(int[][] array2D)
-{ return null; }
+{ 
+   int nonzero = numNonZeroRows(array2D);
+   int[][] smaller = new int[nonzero][array2D[0].length];
+   for(int i = 0; i < array2D.length; i++)
+   {
+      if(isNonZeroRow(array2D, i))
+      {
+         for(int k : array2D[i])
+         {
+            smaller[k] = array2D[]
+         }
+      }
+   }
+ }
 }
